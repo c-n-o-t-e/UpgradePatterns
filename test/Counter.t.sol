c4c3507214e98439853d1dc639fa1408f546f937
c-note <dongustavo20@gmail.com>
2025-01-27 06:58:10 +0000

    Update test cases.

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -1,24 +1,50 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Test, console} from "forge-std/Test.sol";
 import {Counter} from "../src/Counter.sol";
+import {Test, console} from "forge-std/Test.sol";
+import {CounterUpgrade} from "../src/CounterUpgrade.sol";
+import "zeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import {BasicBeaconFactory} from "../src/Beacon/BasicBeaconFactory.sol";
 
 contract CounterTest is Test {
-    Counter public counter;
+    Counter counter;
+    BasicBeaconFactory proxy;
+    BasicBeaconFactory basicBeaconfactory;
 
     function setUp() public {
         counter = new Counter();
-        counter.setNumber(0);
+        basicBeaconfactory = new BasicBeaconFactory();
+
+        bytes memory init = abi.encodeCall(basicBeaconfactory.initialize, (address(counter)));
+        ERC1967Proxy p = new ERC1967Proxy(address(basicBeaconfactory), init);
+        proxy = BasicBeaconFactory(address(p));
     }
 
-    function test_Increment() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
-    }
+    function test_deployProxy() public {
+        CounterUpgrade counterUpgrade = new CounterUpgrade();
 
-    function testFuzz_SetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
+        Counter counter0 = Counter(address(proxy.createBeaconProxy("")));
+        Counter counter1 = Counter(address(proxy.createBeaconProxy("")));
+
+        counter0.increment();
+        counter1.increment();
+        counter1.increment();
+
+        console.log("Counter number: ", counter.number());
+        console.log("Counter number: ", counter0.number());
+        console.log("Counter number: ", counter1.number());
+
+        vm.expectRevert();
+        CounterUpgrade(address(counter0)).decrement();
+
+        vm.prank(address(proxy));
+        proxy.counterBeacon().upgradeTo(address(counterUpgrade));
+
+        CounterUpgrade(address(counter0)).decrement();
+        CounterUpgrade(address(counter1)).decrement();
+
+        console.log("Counter number: ", counter0.number());
+        console.log("Counter number: ", counter1.number());
     }
 }
