031d579eb4c5a832c6378d486d40628880e6915e
c-note <dongustavo20@gmail.com>
2025-01-27 06:19:43 +0000

    rename files.

--- src/Beacon/BasicBeaconFactory.sol
+++ src/Beacon/BasicBeaconFactory.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {BeaconProxy} from "@openzeppelin/contracts/proxy/beacon/BeaconProxy.sol";
-import {UpgradeableBeacon} from "@openzeppelin/contracts/proxy/beacon/UpgradeableBeacon.sol";
+import {BeaconProxy} from "zeppelin/contracts/proxy/beacon/BeaconProxy.sol";
+import {UpgradeableBeacon} from "zeppelin/contracts/proxy/beacon/UpgradeableBeacon.sol";
 
-contract BasicBeacon is UpgradeableBeacon {
+contract BasicBeaconFactory is UpgradeableBeacon {
     constructor(address _implementation) UpgradeableBeacon(_implementation, msg.sender) {}
 
     function createBeaconProxy(bytes memory _data) public returns (BeaconProxy) {
