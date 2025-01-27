f019205ff55d94c119ddc274cb39e833038acbcb
c-note <dongustavo20@gmail.com>
2025-01-27 06:57:50 +0000

    Update factory.

--- src/Beacon/BasicBeaconFactory.sol
+++ src/Beacon/BasicBeaconFactory.sol
@@ -1,14 +1,23 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import {Initializable} from "open/contracts/proxy/utils/Initializable.sol";
 import {BeaconProxy} from "zeppelin/contracts/proxy/beacon/BeaconProxy.sol";
 import {UpgradeableBeacon} from "zeppelin/contracts/proxy/beacon/UpgradeableBeacon.sol";
 
-contract BasicBeaconFactory is UpgradeableBeacon {
-    constructor(address _implementation) UpgradeableBeacon(_implementation, msg.sender) {}
+contract BasicBeaconFactory is Initializable {
+    UpgradeableBeacon public counterBeacon;
+
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize(address _implementation) public initializer {
+        counterBeacon = new UpgradeableBeacon(_implementation, msg.sender);
+    }
 
     function createBeaconProxy(bytes memory _data) public returns (BeaconProxy) {
-        BeaconProxy beaconProxy = new BeaconProxy(address(this), _data);
+        BeaconProxy beaconProxy = new BeaconProxy(address(counterBeacon), _data);
         return beaconProxy;
     }
 }
