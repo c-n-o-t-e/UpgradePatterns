92cf493fb879807a87cd743910f59befe5ea9828
c-note <dongustavo20@gmail.com>
2025-01-28 18:33:08 +0000

    Update script

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -1,19 +1,41 @@
-// // SPDX-License-Identifier: UNLICENSED
-// pragma solidity ^0.8.13;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
 
-// import {Script, console} from "forge-std/Script.sol";
-// import {Counter} from "../src/Counter.sol";
+import {Counter} from "../src/Counter.sol";
+import {Script, console} from "forge-std/Script.sol";
+// import {console} from "forge-std/Test.sol";
+import {CounterUpgrade} from "../src/CounterUpgrade.sol";
+import "zeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import {BasicBeaconFactory} from "../src/Beacon/BasicBeaconFactory.sol";
 
-// contract CounterScript is Script {
-//     Counter public counter;
+contract CounterScript is Script {
+    Counter public counter;
+    CounterUpgrade public counterUpgrade;
+    BasicBeaconFactory proxy;
+    BasicBeaconFactory basicBeaconfactory;
 
-//     function setUp() public {}
+    function setUp() public {}
 
-//     function run() public {
-//         vm.startBroadcast();
+    function run() public {
+        uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
 
-//         counter = new Counter();
+        counter = new Counter();
+        counterUpgrade = new CounterUpgrade();
+        basicBeaconfactory = new BasicBeaconFactory();
 
-//         vm.stopBroadcast();
-//     }
-// }
+        bytes memory init = abi.encodeCall(basicBeaconfactory.initialize, (address(counter)));
+        ERC1967Proxy p = new ERC1967Proxy(address(basicBeaconfactory), init);
+        proxy = BasicBeaconFactory(address(p));
+
+        console.log("Counter address: ", address(counter), " Proxy address: ", address(proxy));
+        console.log(
+            "basicBeaconfactory address: ",
+            address(basicBeaconfactory),
+            "CounterUpgrade address: ",
+            address(counterUpgrade)
+        );
+
+        vm.stopBroadcast();
+    }
+}
